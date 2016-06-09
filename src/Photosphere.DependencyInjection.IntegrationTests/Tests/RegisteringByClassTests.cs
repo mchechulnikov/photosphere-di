554ff519f9a88f0257db3030a8daed2583130a7f
chech <ilfirin.nar@gmail.com>
2016-06-09 22:13:19 +0300

    registering by class test fixed #2

---
+++ src/Photosphere.DependencyInjection.IntegrationTests/Tests/RegisteringByClassTests.cs
@@ -0,0 +1,18 @@
+﻿using Photosphere.DependencyInjection.IntegrationTests.TestObjects.Objects;
+using Xunit;
+
+namespace Photosphere.DependencyInjection.IntegrationTests.Tests
+{
+    public class RegisteringByClassTests
+    {
+        [Fact]
+        internal void GetInstance_ByClass_NotNull()
+        {
+            var contaner = new DependencyContainer();
+
+            var foo = contaner.GetInstance<AlwaysNewFoo>();
+
+            Assert.NotNull(foo);
+        }
+    }
+}
