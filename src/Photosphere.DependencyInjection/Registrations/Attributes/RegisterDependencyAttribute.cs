cab6556fabefc930e071f196eb70f2edb1e38c70
sunloving <ilfirin.nar@gmail.com>
2016-07-12 00:16:26 +0300

    Added RegisterDependencyAttribute #12

---
+++ src/Photosphere.DependencyInjection/Registrations/Attributes/RegisterDependencyAttribute.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Photosphere.DependencyInjection.Lifetimes;
+
+namespace Photosphere.DependencyInjection.Registrations.Attributes
+{
+    public class RegisterDependencyAttribute : Attribute
+    {
+        public RegisterDependencyAttribute(Type serviceType, Lifetime lifetime = Lifetime.PerRequest)
+        {
+            ServiceType = serviceType;
+            Lifetime = lifetime;
+        }
+
+        public Type ServiceType { get; }
+
+        public Lifetime Lifetime { get; }
+    }
+}
