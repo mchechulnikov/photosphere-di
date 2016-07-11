44c8371762da036d0bf1b8485d08fb6b447523e8
sunloving <ilfirin.nar@gmail.com>
2016-07-12 00:07:44 +0300

    Added CompositionRootAttribute #12

---
+++ src/Photosphere.DependencyInjection/Registrations/Attributes/CompositionRootAttribute.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace Photosphere.DependencyInjection.Registrations.Attributes
+{
+    public class CompositionRootAttribute : Attribute
+    {
+        public CompositionRootAttribute(Type compositionRootType)
+        {
+            Validate(compositionRootType);
+            CompositionRootType = compositionRootType;
+        }
+
+        public Type CompositionRootType { get; }
+
+        private static void Validate(Type type)
+        {
+            if (type.GetInterfaces().Contains(typeof(ICompositionRoot)))
+            {
+                throw new ArgumentException($"Provided type `{type.FullName}` not implements {nameof(ICompositionRoot)}");
+            }
+        }
+    }
+}
