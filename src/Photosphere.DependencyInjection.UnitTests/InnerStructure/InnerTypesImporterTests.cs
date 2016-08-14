551b393d817a747346e32cf951cf0f2489af3099
sunloving <ilfirin.nar@gmail.com>
2016-08-14 11:55:13 +0300

    Parse ctors #24

--- src/Photosphere.DependencyInjection.UnitTests/InnerStructure/InnerTypesImporterTests.cs
+++ src/Photosphere.DependencyInjection.UnitTests/InnerStructure/InnerTypesImporterTests.cs
@@ -1,4 +1,4 @@
-﻿using System.IO;
+﻿using System.Linq;
 using System.Text.RegularExpressions;
 using Xunit;
 
@@ -11,8 +11,7 @@ namespace Photosphere.DependencyInjection.UnitTests.InnerStructure
         [Fact]
         internal void Test1()
         {
-            Directory.GetFiles("dfd", "df", SearchOption.AllDirectories);
-            var value = new Regex("class [a-zA-Z0-9]+").Match("dfd").Value;
+            var value = new Regex("class [a-zA-Z0-9]+").Match(TypeDefinitionExample).Value;
             var actualString = value.Substring(6);
             Assert.Contains("Foo", actualString);
         }
@@ -27,6 +26,30 @@ namespace Photosphere.DependencyInjection.UnitTests.InnerStructure
                     new Regex(":( )*([a-zA-Z0-9]+(, )*)+")
                     .Match(TypeDefinitionExample).Value
                     .TrimStart(':').TrimStart().Replace(",", string.Empty).Split(' '));
+            }
+        }
+
+        [Fact]
+        internal void Test3()
+        {
+            const string str = @"
+                public class Foo : FooBase, IFoo, IBar
+                {
+                  public Foo(this IBuz buz, IQiz qiz = null)
+                  {
+                  }
+                }";
+            var expected = new [] { "IBuz", "IQiz" };
+            var result = new Regex("Foo\\(([(this) \\w=]+,*( )*)+")
+                .Match(str).Value
+                .Replace("Foo(", string.Empty)
+                .Replace("this ", string.Empty)
+                .Split(',')
+                .Select(x => x.Trim())
+                .Select(x => x.Split(' ').First());
+            foreach (var s in expected)
+            {
+                Assert.Contains(s, result);
             }
         }
     }
